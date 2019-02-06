9b9959c2cdb9b82152aabb225f17f0f8dee4bb32
Caleb Taylor <02caleb.taylor@gulllakecs.org>
2019-02-06 10:01:46 -0800

    We added a Greetings Function

---
+++ Functions- git.py
@@ -0,0 +1,11 @@
+# 4.13.3: Greetings
+# Caleb Taylor
+# 2.5.19
+
+name = input("What's your name: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you")
+
+greeting()
